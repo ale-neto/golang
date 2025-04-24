c684db6bebb1679de2d02aa7eb32106f140a4eb6
Alexandre Neto <alexandreneto.dev@gmail.com>
2025-04-24 00:07:15 -0300

    #2 - standardizing errors

---
+++ src/config/err/err.go
@@ -0,0 +1,82 @@
+package err
+
+import "net/http"
+
+type Err struct {
+	Message string   `json:"message"`
+	Err     string   `json:"err"`
+	Code    int64    `json:"code"`
+	Causes  []Causes `json:"causes"`
+}
+
+type Causes struct {
+	Field   string `json:"field"`
+	Message string `json:"message"`
+}
+
+func (e *Err) Error() string {
+	return e.Message
+}
+
+func NewErr(message string, err string, code int64, causes []Causes) *Err {
+	return &Err{
+		Message: message,
+		Err:     err,
+		Code:    code,
+		Causes:  causes,
+	}
+}
+
+func NewBadRequestErr(message string, causes []Causes) *Err {
+	return &Err{
+		Message: message,
+		Err:     "bad request",
+		Code:    http.StatusBadRequest,
+		Causes:  causes,
+	}
+}
+
+func NewBadRequestValidationErr(message string, causes []Causes) *Err {
+	return &Err{
+		Message: message,
+		Err:     "bad request",
+		Code:    http.StatusBadRequest,
+		Causes:  causes,
+	}
+}
+
+func NewInternalServerErr(message string, causes []Causes) *Err {
+	return &Err{
+		Message: message,
+		Err:     "internal server error",
+		Code:    http.StatusInternalServerError,
+		Causes:  causes,
+	}
+}
+
+func NewNotFoundErr(message string, causes []Causes) *Err {
+	return &Err{
+		Message: message,
+		Err:     "not found",
+		Code:    http.StatusNotFound,
+		Causes:  causes,
+	}
+}
+
+func NewUnauthorizedErr(message string, causes []Causes) *Err {
+	return &Err{
+		Message: message,
+		Err:     "unauthorized",
+		Code:    http.StatusUnauthorized,
+		Causes:  causes,
+	}
+}
+
+func NewForbiddenErr(message string, causes []Causes) *Err {
+	return &Err{
+		Message: message,
+		Err:     "forbidden",
+		Code:    http.StatusForbidden,
+		Causes:  causes,
+	}
+}
