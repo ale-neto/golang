478969b115e498db71f94e6ff9c0dc588480577b
Alexandre Neto <alexandreneto.dev@gmail.com>
2025-04-27 23:56:11 -0300

    #5 - Create new's model Request e Response by User

---
+++ src/controller/model/response/userResponse.go
@@ -0,0 +1,9 @@
+package response
+
+type UserResponse struct {
+	ID       string `json:"id"`
+	Username string `json:"username"`
+	Email    string `json:"email"`
+	Name     string `json:"name"`
+	Age      int    `json:"age"`
+}
