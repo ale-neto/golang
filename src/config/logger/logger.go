d0c71e957936b0f59a4851626b5035c2e88da0cd
Alexandre Neto <alexandreneto.dev@gmail.com>
2025-05-02 23:48:45 -0300

    #10 - Final configuration for the logger

--- src/config/logger/logger.go
+++ src/config/logger/logger.go
@@ -1,27 +1,46 @@
 package logger
 
-var (
-	log *zap.Logger
+import (
+	"os"
+	"strings"
 
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+var (
+	log        *zap.Logger
 	LOG_OUTPUT = "LOG_OUTPUT"
 	LOG_LEVEL  = "LOG_LEVEL"
 )
 
-func init () {
-	logConfig := zap.NewConfig(){
+func init() {
+	logConfig := zap.Config{
 		OutputPaths: []string{getOutputLogs()},
-		Level:       zap.NewAtomicLevelAt(getgetLevelLogs()),
-		Encoding: zapcore.EncoderConfig{
-			Levelkey:    "level",
-			TimeKey:     "time",
-			MessageKey: "message",
-			EncondeTime: zapcore.ISO8601TimeEncoder,
-			EncodeLevel: zapcore.CapitalLevelEncoder,
-			EncodeDuration: zapcore.StringDurationEncoder,
+		Level:       zap.NewAtomicLevelAt(getLevelLogs()),
+		Encoding:    "json",
+		EncoderConfig: zapcore.EncoderConfig{
+			LevelKey:     "level",
+			TimeKey:      "time",
+			MessageKey:   "message",
+			EncodeTime:   zapcore.ISO8601TimeEncoder,
+			EncodeLevel:  zapcore.LowercaseLevelEncoder,
 			EncodeCaller: zapcore.ShortCallerEncoder,
-			
-		}
+		},
 	}
+
+	log, _ = logConfig.Build()
+}
+
+func Info(message string, tags ...zap.Field) {
+	log.Info(message, tags...)
+	log.Sync()
+}
+
+func Error(message string, err error, tags ...zap.Field) {
+	tags = append(tags, zap.NamedError("error", err))
+	log.Info(message, tags...)
+	log.Sync()
 }
 
 func getOutputLogs() string {
