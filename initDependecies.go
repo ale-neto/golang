5524dc29dfbddd0027c8071a1d668193b996ab9e
Alexandre Neto <alexandreneto.dev@gmail.com>
2025-05-14 23:17:14 -0300

    #22 - Final Repository CreatingUser

--- initDependecies.go
+++ initDependecies.go
@@ -4,11 +4,11 @@ import (
 	"github.com/ale-neto/golang/src/controller"
 	"github.com/ale-neto/golang/src/model/repository"
 	"github.com/ale-neto/golang/src/model/service"
+	"go.mongodb.org/mongo-driver/v2/mongo"
 )
 
-func initDependencies() (controller.UserControllerInterface, error) {
+func initDependencies(database *mongo.Database) controller.UserControllerInterface {
 	repo := repository.NewUserRepository(database)
 	service := service.NewUserDomainService(repo)
-	userController := controller.NewUserControllerInterface(service)
-
+	return controller.NewUserControllerInterface(service)
 }
