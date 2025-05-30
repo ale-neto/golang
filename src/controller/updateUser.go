41c0e9f235b287741aabf6dde3a79c6890380f7b
Alexandre Neto <alexandreneto.dev@gmail.com>
2025-05-29 23:47:10 -0300

    #38 - Added initial for Update User

--- src/controller/updateUser.go
+++ src/controller/updateUser.go
@@ -1,7 +1,57 @@
 package controller
 
 import (
+	"net/http"
+
+	err_rest "github.com/ale-neto/golang/src/config/err"
+	"github.com/ale-neto/golang/src/config/logger"
+	"github.com/ale-neto/golang/src/config/validation"
+	"github.com/ale-neto/golang/src/controller/model/request"
+	"github.com/ale-neto/golang/src/model"
 	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/v2/bson"
+	"go.uber.org/zap"
 )
 
-func (u *userControllerInterface) UpdateUser(c *gin.Context) {}
+func (u *userControllerInterface) UpdateUser(c *gin.Context) {
+	logger.Info("Init updateUser controller",
+		zap.String("journey", "updateUser"),
+	)
+	var userRequest request.UserUpdateRequest
+
+	if err := c.ShouldBindJSON(&userRequest); err != nil {
+		logger.Error("Error trying to validate user info", err,
+			zap.String("journey", "updateUser"))
+		errRest := validation.ValidateUserError(err)
+
+		c.JSON(errRest.Code, errRest)
+		return
+	}
+
+	userId := c.Param("userId")
+	if _, err := bson.ObjectIDFromHex(userId); err != nil {
+		errRest := err_rest.NewBadRequestErr("Invalid userId, must be a hex value")
+		c.JSON(errRest.Code, errRest)
+	}
+
+	domain := model.NewUserUpdateDomain(
+		userRequest.Name,
+		userRequest.Age,
+	)
+	err := u.service.UpdateUser(userId, domain)
+	if err != nil {
+		logger.Error(
+			"Error trying to call updateUser service",
+			err,
+			zap.String("journey", "updateUser"))
+		c.JSON(err.Code, err)
+		return
+	}
+
+	logger.Info(
+		"updateUser controller executed successfully",
+		zap.String("userId", userId),
+		zap.String("journey", "updateUser"))
+
+	c.Status(http.StatusOK)
+}
--- src/model/service/updateUser.go
+++ src/model/service/updateUser.go
@@ -2,9 +2,29 @@ package service
 
 import (
 	err_rest "github.com/ale-neto/golang/src/config/err"
+	"github.com/ale-neto/golang/src/config/logger"
 	"github.com/ale-neto/golang/src/model"
+	"go.uber.org/zap"
 )
 
-func (u *userDomainService) UpdateUserService(id string, user model.UserDomainInterface) *err_rest.Err {
+func (u *userDomainService) UpdateUser(
+	userId string,
+	userDomain model.UserDomainInterface,
+) *err_rest.Err {
+	logger.Info("Init updateUser model.",
+		zap.String("journey", "updateUser"))
+
+	err := u.userRepository.UpdateUser(userId, userDomain)
+	if err != nil {
+		logger.Error("Error trying to call repository",
+			err,
+			zap.String("journey", "updateUser"))
+		return err
+	}
+
+	logger.Info(
+		"updateUser service executed successfully",
+		zap.String("userId", userId),
+		zap.String("journey", "updateUser"))
 	return nil
 }
